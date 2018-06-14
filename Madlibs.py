4356796e1921bec4d330fab1572a24f7a83a7a1c
Rakshit Malhotra <39698255+Raks-coder@users.noreply.github.com>
2018-06-14 19:34:40 +0530

    Create Madlibs.py
    Nice fun game kindda thing

---
+++ Madlibs.py
@@ -0,0 +1,40 @@
+"""This project basically uses mad-lib
+
+"""
+
+# The template for the story
+
+"""STORY = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %ss were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %ss very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %ss ruled the world."""
+
+print("Mad Libs has started")
+name=raw_input("Enter a name:")
+Adjective1=raw_input("Enter adjective1:")
+Adjective2=raw_input("Enter adjective2:")
+Adjective3=raw_input("Enter adjective3:")
+verb=raw_input("Enter a verb:")
+noun1=raw_input("Enter a noun:")
+noun2=raw_input("Enter another noun:")
+animal=raw_input("Enter an animal:")
+food=raw_input("Enter a food:")
+fruit=raw_input("Enter a fruit:")
+superhero=raw_input("Enter a superhero:")
+country=raw_input("Enter a country:")
+dessert=raw_input("Enter a dessert:")
+year=raw_input("Enter a year:")
+
+STORY = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %ss were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %ss very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %ss ruled the world."
+print STORY %(name, Adjective1, Adjective2, animal, food, verb, noun1, fruit, Adjective3, name, superhero, name, country, name, dessert, name, year, noun2)
+
+
+
+
+
+#My friends using this code please use python Madlibs.py to run this in the terminal...this one is quite fun
+
+
+
+
+
+
+
+
